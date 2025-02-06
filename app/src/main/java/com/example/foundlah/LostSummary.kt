b9bc345c383ac188366df8f254027e5bf382b8fc
shan <shankarshan2468@gmail.com>
2025-02-06 23:14:55 +0800

    updated update for UI

--- app/src/main/java/com/example/foundlah/LostSummary.kt
+++ app/src/main/java/com/example/foundlah/LostSummary.kt
@@ -1,5 +1,6 @@
 package com.example.foundlah
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
--- app/src/main/java/com/example/foundlah/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.example.foundlah.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult (
-     val success:LoggedInUserView? = null,
-     val error:Int? = null
-)
