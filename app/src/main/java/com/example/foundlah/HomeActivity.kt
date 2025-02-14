7680a4bde4b7ebc9b2e6c40a8f682f64bcb6cf52
Pari Sivaji <155886505+SyntaxTerror2000@users.noreply.github.com>
2025-02-14 12:58:30 +0800

    Added functionality to buttons

--- app/src/main/java/com/example/foundlah/HomeActivity.kt
+++ app/src/main/java/com/example/foundlah/HomeActivity.kt
@@ -1,12 +1,12 @@
 package com.example.foundlah
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
--- app/src/main/java/com/example/foundlah/Page1.kt
+++ app/src/main/java/com/example/foundlah/Page1.kt
@@ -1,12 +1,14 @@
 package com.example.foundlah
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Page1 : AppCompatActivity() {
+class Page1 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +17,13 @@ class Page1 : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val loginButton: Button = findViewById<Button>(R.id.button)
+
+        loginButton.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
         }
     }
 }
