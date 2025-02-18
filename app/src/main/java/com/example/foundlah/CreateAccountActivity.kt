caae73322bf950dfb2e35833613138319f6636e2
JORDAN\jorda <jordan.lee32@yahoo.com.sg>
2025-02-19 00:48:27 +0800

    Rename activity to "Login" and edited constraints, id, UI

--- app/src/main/java/com/example/foundlah/CreateAccountActivity.kt
+++ app/src/main/java/com/example/foundlah/CreateAccountActivity.kt
@@ -1,12 +1,28 @@
 package com.example.foundlah
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 
 class CreateAccountActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val backText = findViewById<TextView>(R.id.textBack)
+
+        backText.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/foundlah/Login.kt
+++ app/src/main/java/com/example/foundlah/Login.kt
@@ -4,25 +4,32 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Page1 : ComponentActivity() {
+class Login : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_page1)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val loginButton: Button = findViewById<Button>(R.id.button)
+        val loginButton = findViewById<Button>(R.id.button)
+        val createAccount = findViewById<TextView>(R.id.textCreateAccount)
 
         loginButton.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        createAccount.setOnClickListener {
+            val intent = Intent(this, CreateAccountActivity::class.java)
             startActivity(intent)
         }
     }
